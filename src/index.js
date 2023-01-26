b963a768afb62f1b1e5d53e5dcbeba5544586f61
Vadym <vadymrusyn92@gmail.com>
2023-01-26 16:04:12 +0200

    feat: trying to complate task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []
+  const closeBrackets = []
+  const bracketsPair = {}
+  bracketsConfig.forEach(elem => bracketsPair[elem[1]] = elem[0])
+  bracketsConfig.forEach(elem => closeBrackets.push(elem[1]))
+  for (let i = 0; i < str.length; i++) {
+    let currentSumbol = str[i];
+    console.log(currentSumbol)
+    if (closeBrackets.includes(currentSumbol)) {
+      if (bracketsPair[currentSumbol] !== stack.pop()) return false;
+    } else {
+      stack.push(currentSumbol);
+    }
+
+  }
+
+  return stack.length === 0;
 }
+
+// console.log(check('(||)', [['(', ')'], ['|', '|']]));
+
+
+// module.exports = function check(str, bracketsConfig) {
+//   const stack = []
+//   const closeBrackets = []
+//   const bracketsPair = {}
+//   bracketsConfig.forEach(elem => bracketsPair[elem[1]] = elem[0])
+//   bracketsConfig.forEach(elem => closeBrackets.push(elem[1]))
+//   for (let i = 0; i < str.length; i++) {
+//     let currentSumbol = str[i];
+//     console.log(currentSumbol)
+//     if (closeBrackets.includes(currentSumbol)) {
+//       if (bracketsPair[currentSumbol] !== stack.pop()) return false;
+//     } else {
+//       stack.push(currentSumbol);
+//     }
+
+//   }
+
+//   return stack.length === 0;
+// }
